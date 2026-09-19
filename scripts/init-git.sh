35aecfe925116f7e76afe6f663eea81c6a922c75
Ivory47 <148820959+Ivory47@users.noreply.github.com>
2026-09-19 20:58:58 +0200

    chose the correct email

--- scripts/init-git.sh
+++ scripts/init-git.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 git config --global user.name "Ivory47"
-git config --global user.email "example@email.com"
+git config --global user.email "148820959+Ivory47@users.noreply.github.com"
